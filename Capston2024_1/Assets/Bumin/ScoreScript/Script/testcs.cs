083a6cc52c6ad96a6d800e267f9fe38af6cf45a3
kimbumin <113817229+kimbumin@users.noreply.github.com>
2024-05-11 12:26:45 +0900

    update test.cs

--- Capston2024_1/Assets/Bumin/ScoreScript/Script/testcs.cs
+++ Capston2024_1/Assets/Bumin/ScoreScript/Script/testcs.cs
@@ -5,6 +5,12 @@ using DG.Tweening;
 
 public class testcs : MonoBehaviour
 {
+    public FingerPrintObject fingerprintobject;
+
+    private void Start()
+    {
+        fingerprintobject = GetComponent<FingerPrintObject>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,8 +20,10 @@ public class testcs : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
-            this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(0f, 0.4f);
+        if (fingerprintobject.isVisible == false) {
+            if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
+                this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(0f, 0.4f);
+        }
     }
 
 }
