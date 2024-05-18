d1f79d7dcbca3ef748dbe6b8e6d6ae61c9f844ce
kimbumin <113817229+kimbumin@users.noreply.github.com>
2024-05-18 23:16:43 +0900

    packingEvidens 스크립트 수정 실험

--- Capston2024_1/Assets/MIna/Script/Collecting/PackingEvidence.cs
+++ Capston2024_1/Assets/MIna/Script/Collecting/PackingEvidence.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PackingEvidence : MonoBehaviour
@@ -11,15 +10,19 @@ public class PackingEvidence : MonoBehaviour
 
     public void PackEvidence()
     {
-
-        //컨트롤러 Y버튼 누르면 증거물 packed
+        // 컨트롤러 Y버튼 누르면 증거물 packed
         if (OVRInput.GetDown(OVRInput.Button.Four))
         {
+            Debug.Log("Y 버튼 눌림, 증거물 밀봉 시작");
+
             PackedObj.transform.position = EvidenceObj.transform.position;
             PackedObj.transform.rotation = EvidenceObj.transform.rotation;
 
             EvidenceObj.SetActive(false);
+            Debug.Log("EvidenceObj 비활성화");
+
             PackedObj.SetActive(true);
+            Debug.Log("PackedObj 활성화");
 
             SoundManager.Instance.PlaySFX(SoundManager.SFX_list.FLAP_2); // 사운드 추가 - 정현우
         }
@@ -28,21 +31,21 @@ public class PackingEvidence : MonoBehaviour
     public void OnGrab()
     {
         isGrabbed = true;
+        StartCoroutine(CheckGrabbed());
     }
 
     public void OffGrab()
     {
         isGrabbed = false;
+        StopCoroutine(CheckGrabbed());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator CheckGrabbed()
     {
-        if (isGrabbed == true)
+        while (isGrabbed)
         {
             PackEvidence();
+            yield return null; // 다음 프레임까지 대기
         }
     }
-
-
 }
