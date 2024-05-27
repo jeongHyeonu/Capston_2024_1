9787503a2b1b87badf338dbeffdc80a9dcac2101
Yanghyeonyong <127082677+Yanghyeonyong@users.noreply.github.com>
2024-05-27 19:35:54 +0900

    튜토리얼 손전등 수정
    변경된 씬
    - Tutorial
    
    변경된 스크립트
    - testcs.cs

--- Capston2024_1/Assets/Bumin/ScoreScript/Script/testcs.cs
+++ Capston2024_1/Assets/Bumin/ScoreScript/Script/testcs.cs
@@ -7,22 +7,59 @@ public class testcs : MonoBehaviour
 {
     public FingerPrintObject fingerprintobject;
 
+
+
+    //0527 양현용 추가
+    public FingerPrintObject_tutorial ft;
+    public GameObject FingerPrint;
+    public bool onTutorial = false;
+
     private void Start()
     {
-        fingerprintobject = GetComponent<FingerPrintObject>();
+        if (onTutorial == false)
+        {
+            fingerprintobject = GetComponent<FingerPrintObject>();
+        }
+        if (onTutorial == true) { 
+            ft=FingerPrint.GetComponent<FingerPrintObject_tutorial>();       
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
-            this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(1f, 0.4f);
+        if (onTutorial == false)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
+                this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(1f, 0.4f);
+        }
+        if (onTutorial == true)
+        {
+            if (ft.isVisible == false)
+            {
+                if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
+                    this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(1f, 0.4f);
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (fingerprintobject.isVisible == false) {
-            if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
-                this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(0f, 0.4f);
+        if (onTutorial == false)
+        {
+            if (fingerprintobject.isVisible == false)
+            {
+                if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
+                    this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(0f, 0.4f);
+            }
+        }
+
+        if(onTutorial == true)
+        {
+            if (ft.isVisible == false)
+            {
+                if (other.gameObject.layer == LayerMask.NameToLayer("UV_Light"))
+                    this.transform.gameObject.GetComponent<MeshRenderer>().materials[0].DOFade(0f, 0.4f);
+            }
         }
     }
 
